8ad6ecab221ec08b340e9bde0ba77e9006b6f331
Ryan Leung <cygnusdoodle@hotmail.com>
2021-04-25 01:15:25 -0400

    Falling Spikes

---
+++ Assets/Traps/Crackable.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crackable : MonoBehaviour {
+    [SerializeField] GameObject CrackedVersion = null;
+
+    List<MeshRenderer> MeshRenderers = new List<MeshRenderer>();
+
+    public void Crack() {
+
+        // 3D Objs
+        Vector3 targetPos = transform.position;
+        targetPos.z -= 0.1f;
+
+        GameObject obj = Instantiate(CrackedVersion, targetPos, CrackedVersion.transform.rotation);
+
+        Destroy(obj, 1f);
+    }
+
+}
---
+++ Assets/Traps/FallingSpike/FallingSpike.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingSpike : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb2 = null;
+    [SerializeField] SpriteRenderer SpriteRenderer = null;
+    [SerializeField] Crackable Crackable = null;
+
+    public LayerMask LayerMask;
+
+    Vector3 originalPos;
+    bool Respawning = false;
+
+    private void Start() {
+        originalPos = transform.position;
+    }
+
+    void FixedUpdate() {
+
+        if (!Respawning) {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector3.up, Mathf.Infinity, LayerMask);
+
+            if (hit.collider != null) {
+                rb2.gravityScale = 1;
+            }
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision) {
+        Crackable.Crack();
+
+        StartCoroutine(Respawn());
+    }
+
+    public IEnumerator Respawn() {
+        Respawning = true;
+
+        Color color = SpriteRenderer.color;
+        color.a = 0;
+
+        SpriteRenderer.color = color;
+
+        rb2.gravityScale = 0;
+        transform.position = originalPos;
+
+        yield return new WaitForSeconds(1f);
+
+        while (color.a < 1) {
+            color.a += Time.deltaTime;
+
+            SpriteRenderer.color = color;
+
+            yield return null;
+        }
+
+        Respawning = false;
+    }
+}
